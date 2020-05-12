547cbf456f3c9904b6784420e779913f40833186
Lenny Makori <makorilenny28@gmail.com>
2020-05-12 08:05:19 +0300

    create auth folder

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,19 @@
 from flask import Flask
-
+from config import config_options
 
 def create_app(config_name):
     app = Flask(__name__)
 
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    #initializing flask extensions
     
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+
+    return app
+
